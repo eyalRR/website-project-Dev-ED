5679da3c18535705a609d0f2662b4a2157af1f85
Eyal <rabeyal123@gmail.com>
2022-02-27 02:35:40 +0200

    another page

--- app.js
+++ app.js
@@ -60,8 +60,9 @@ function animateSlides(){
     });
 }
 
-let mouse = document.querySelector('.cursor');
-let mouseTxt = mouse.querySelector('span');
+const mouse = document.querySelector('.cursor');
+const mouseTxt = mouse.querySelector('span');
+const burger = document.querySelector(".burger");
 function cursor(e){
     mouse.style.top = e.pageY + "px";
     mouse.style.left = e.pageX + "px";
@@ -83,6 +84,26 @@ function activeCursor(e){
         gsap.to('.title-swipe', 1, {y: '100%'});
     }
 }
+function navToggle(e){
+    if(!e.target.classList.contains("active")){
+        e.target.classList.add("active")
+        gsap.to(".line1", 0.5, {rotate: "45", y: 5, background: "black"});
+        gsap.to(".line2", 0.5, {rotate: "-45", y: -5, background: "black"});
+        gsap.to("#logo", 1, {color: "black"});
+        gsap.to(".nav-bar", 1, {clipPath: "circle(2500px at 100% -10%)"});
+        document.body.classList.add("hide");
+    } else{
+        e.target.classList.remove("active")
+        gsap.to(".line1", 0.5, {rotate: "0", y: 0, background: "white"});
+        gsap.to(".line2", 0.5, {rotate: "0", y: 0, background: "white"});
+        gsap.to("#logo", 1, {color: "white"});
+        gsap.to(".nav-bar", 1, {clipPath: "circle(50px at 100% -10%)"});
+        document.body.classList.remove("hide");
+    }
+    
+}
+// Event Listeners
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
 
